dd1f483c86f51c799a1e0ed5590c2454da9a8e4d
markusbautista <141809532+markusbautista@users.noreply.github.com>
2023-12-11 07:31:33 +0800

    Create science.js

---
+++ q_categories/science.js
@@ -0,0 +1,54 @@
+const science_questions = [
+  {
+    question: "What is the largest planet in our solar system?",
+    answer: "Jupiter",
+    category: "Science"
+  },
+  {
+    question: "Which galaxy is the Milky Way destined to collide with in the distant future?",
+    answer: "Andromeda Galaxy",
+    category: "Science"
+  },
+  {
+    question: "What is the name of the phenomenon where a star explodes, releasing an enormous amount of energy?",
+    answer: "Supernova",
+    category: "Science"
+  },
+  {
+    question: "Which planet is known as the 'Red Planet'?",
+    answer: "Mars",
+    category: "Science"
+  },
+  {
+    question: "What is the distance between the Earth and the Sun?",
+    answer: "Approximately 93 million miles (150 million kilometers)",
+    category: "Science"
+  },
+  {
+    question: "Who was the first human to walk on the Moon?",
+    answer: "Neil Armstrong",
+    category: "Science"
+  },
+  {
+    question: "What is the name of the bright, giant star at the center of our solar system?",
+    answer: "The Sun",
+    category: "Science"
+  },
+  {
+    question: "Which famous telescope was launched into space in 1990 and has provided breathtaking images of the universe?",
+    answer: "Hubble Space Telescope",
+    category: "Science"
+  },
+  {
+    question: "What is the Great Red Spot on Jupiter?",
+    answer: "A giant storm",
+    category: "Science"
+  },
+  {
+    question: "What is the name of the closest galaxy to the Milky Way?",
+    answer: "Canis Major Dwarf Galaxy",
+    category: "Science"
+  }
+];
+
+export default science_questions;
