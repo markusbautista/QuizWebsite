a98eced2226a1865b631f5541a8c812b0cd4842a
markusbautista <141809532+markusbautista@users.noreply.github.com>
2023-12-11 07:32:02 +0800

    Add files via upload

---
+++ q_categories/history.js
@@ -0,0 +1,54 @@
+const history_questions = [
+  {
+    question: "Who was the first President of the United States?",
+    answer: "George Washington",
+    category: "History"
+  },
+  {
+    question: "In which year did Christopher Columbus first reach the Americas?",
+    answer: "1492",
+    category: "History"
+  },
+  {
+    question: "What ancient civilization built the famous city of Machu Picchu in the 15th century?",
+    answer: "Incas",
+    category: "History"
+  },
+  {
+    question: "The Magna Carta, signed in 1215, is often regarded as a key document in the development of what?",
+    answer: "Democracy",
+    category: "History"
+  },
+  {
+    question: "Who was the leader of the Soviet Union during World War II?",
+    answer: "Joseph Stalin",
+    category: "History"
+  },
+  {
+    question: "Which war is often referred to as the 'War of 1812'?",
+    answer: "Anglo-American War",
+    category: "History"
+  },
+  {
+    question: "What event marked the beginning of World War I in 1914?",
+    answer: "Assassination of Archduke Franz Ferdinand",
+    category: "History"
+  },
+  {
+    question: "Who was the famous queen of ancient Egypt known for her alliance with Julius Caesar and Mark Antony?",
+    answer: "Cleopatra",
+    category: "History"
+  },
+  {
+    question: "The Renaissance, a period of renewed interest in art and learning, originated in which country?",
+    answer: "Italy",
+    category: "History"
+  },
+  {
+    question: "What was the main cause of the Great Depression in the 1930s?",
+    answer: "Stock market crash",
+    category: "History"
+  }
+];
+
+export default history_questions;
