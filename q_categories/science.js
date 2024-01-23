1d89d3a5389e803c1f7486971b8babed3930609e
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-01-23 16:51:29 +0800

    Update science.js

--- q_categories/science.js
+++ q_categories/science.js
@@ -1,54 +1,232 @@
-const science_questions = [
+const chapter1Questions = [
   {
-    question: "What is the largest planet in our solar system?",
-    answer: "Jupiter",
-    category: "Science"
+    question: "Is described as any physical component of a computer system that contains a circuit board, Integrated Circuits (ICs), or other electronics. Without any _____, your computer would not function, and software could not be used.",
+    answer: "Hardware",
+    category: "Chapter_1"
   },
   {
-    question: "Which galaxy is the Milky Way destined to collide with in the distant future?",
-    answer: "Andromeda Galaxy",
-    category: "Science"
+    question: "A computer’s _____ is divided into two different components: Input devices and Output devices.",
+    answer: "Hardware",
+    category: "Chapter_1"
   },
   {
-    question: "What is the name of the phenomenon where a star explodes, releasing an enormous amount of energy?",
-    answer: "Supernova",
-    category: "Science"
+    question: "Is a piece of hardware that sends data to a computer. Most _____ either interact with or control the computer in some way.",
+    answer: "Input Devices",
+    category: "Chapter_1"
   },
   {
-    question: "Which planet is known as the 'Red Planet'?",
-    answer: "Mars",
-    category: "Science"
+    question: "An _____ sends data to the computer.",
+    answer: "Input Device",
+    category: "Chapter_1"
   },
   {
-    question: "What is the distance between the Earth and the Sun?",
-    answer: "Approximately 93 million miles (150 million kilometers)",
-    category: "Science"
+    question: "Is a piece of computer hardware that receives data from a computer and then translates that data into another form. That form may be audio, visual, textual, or hard copy such as a printed document.ss",
+    answer: "Output Devices",
+    category: "Chapter_1"
   },
   {
-    question: "Who was the first human to walk on the Moon?",
-    answer: "Neil Armstrong",
-    category: "Science"
+    question: "An _____ receives data from the computer.",
+    answer: "Output Device",
+    category: "Chapter_1"
   },
   {
-    question: "What is the name of the bright, giant star at the center of our solar system?",
-    answer: "The Sun",
-    category: "Science"
+    question: "Also known as a \"tower\" or \"chassis,\" is the main part of a desktop computer. It includes the motherboard, CPU, RAM, and other components.",
+    answer: "System Unit",
+    category: "Chapter_1"
   },
   {
-    question: "Which famous telescope was launched into space in 1990 and has provided breathtaking images of the universe?",
-    answer: "Hubble Space Telescope",
-    category: "Science"
+    question: "The _____ also includes the case that houses the internal components of the computer.",
+    answer: "System Unit",
+    category: "Chapter_1"
   },
   {
-    question: "What is the Great Red Spot on Jupiter?",
-    answer: "A giant storm",
-    category: "Science"
+    question: "Some modern computers, such as the _____, combine the system unit and monitor into a single device. In this case, the monitor is part of the system unit. While laptops also have built-in displays, they are not called system unit, since the term only refers to desktop computers.",
+    answer: "iMac",
+    category: "Chapter_1"
   },
   {
-    question: "What is the name of the closest galaxy to the Milky Way?",
-    answer: "Canis Major Dwarf Galaxy",
-    category: "Science"
+    question: "Gives instructions and commands to a computer. _____ comprises the entire set of programs, procedures, and routines associated with the operation of a computer system.",
+    answer: "Software",
+    category: "Chapter_1"
+  },
+  {
+    question: "The term _____ was coined to differentiate these instructions from hardware—i.e., the physical components of a computer system.",
+    answer: "Software",
+    category: "Chapter_1"
+  },
+  {
+    question: "A set of instructions that directs a computer’s hardware to perform a task is called a program, or software program.",
+    answer: "Program/Software Program",
+    category: "Chapter_1"
+  },
+  {
+    question: "is a computer software package that performs a specific function directly for an end user or, in some cases, for another application.",
+    answer: "Application Software - is a computer software package that performs a specific function directly for an end user or, in some cases, for another application.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Can be self-contained or a group of programs.",
+    answer: "Application - Can be self-contained or a group of programs.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Is a set of operations that runs the application for the user.",
+    answer: "Program - Is a set of operations that runs the application for the user.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Is a type of computer program that is designed to run a computer's hardware and application programs.",
+    answer: "System Software - Is a type of computer program that is designed to run a computer's hardware and application programs.",
+    category: "Chapter_1"
+  },
+  {
+    question: "If we think of the computer system as a layered model, the _____ is the interface between the hardware and user applications.",
+    answer: "System Software - If we think of the computer system as a layered model, the _____ is the interface between the hardware and user applications.",
+    category: "Chapter_1"
+  },
+  {
+    question: "The _____ is the best-known example of system software. It manages all the other programs in a computer.",
+    answer: "Operating System - The _____ is the best-known example of system software. It manages all the other programs in a computer.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Is used to manage the computer itself. It runs in the background, maintaining the computer's basic functions so users can run higher-level application software to perform certain tasks.",
+    answer: "System Software - Is used to manage the computer itself. It runs in the background, maintaining the computer's basic functions so users can run higher-level application software to perform certain tasks.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Provides a platform for application software to be run on top of.",
+    answer: "System Software - Provides a platform for application software to be run on top of.",
+    category: "Chapter_1"
+  },
+  {
+    question: "The user makes use of application software to perform some specific task.",
+    answer: "Application Software - The user makes use of application software to perform some specific task.",
+    category: "Chapter_1"
+  },
+  {
+    question: "We install the application software according to the requirement of the user.",
+    answer: "Application Software - We install the application software according to the requirement of the user.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Generally, the user interacts with the application software.",
+    answer: "Application Software - Generally, the user interacts with the application software.",
+    category: "Chapter_1"
+  },
+  {
+    question: "It can’t run independently because it is not able to run without the presence of system software.",
+    answer: "Application Software - It can’t run independently because it is not able to run without the presence of system software.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Example: word processor, web browser, media player etc.",
+    answer: "Application Software - Example: word processor, web browser, media player etc.",
+    category: "Chapter_1"
+  },
+  {
+    question: "We make use of system software for operating hardware on the computer.",
+    answer: "System Software - We make use of system software for operating hardware on the computer.",
+    category: "Chapter_1"
+  },
+  {
+    question: "When the operating system is installed on the computer, then the system software is also mounted on the machine.",
+    answer: "System Software - When the operating system is installed on the computer, then the system software is also mounted on the machine.",
+    category: "Chapter_1"
+  },
+  {
+    question: "System Software works in the background. So the user is not able to interact with it.",
+    answer: "It can run independently as it provides the platform for running application softwares.",
+    category: "Chapter_1"
+  },
+  {
+    question: "System Software",
+    answer: "Example: compiler, assembler, interpreter etc.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Algorithm",
+    answer: "The word comes from the name of a Persian author, Abu Ja'far Mohammed ibn Musa al Khowarizmi (c. 825 A.O.), who wrote a textbook on mathematics.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Algorithm",
+    answer: "An examination of the latest edition of Webster's dictionary defines its meaning as 'any special method of solving a certain kind of problem.' But this word has taken on a special significance in computer science.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Algorithm",
+    answer: "Has come to refer to a precise method usable by a computer to solve a problem. This is what makes an algorithm different from words such as process, technique, or practice. (Horowitz, Sahni, 1978).",
+    category: "Chapter_1"
+  },
+  {
+    question: "Algorithm",
+    answer: "Informally, an algorithm is any well-defined computational procedure that takes some value, or set of values, as input and produces some value, or set of values, as output.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Algorithm",
+    answer: "An algorithm is thus a sequence of computational steps that transform the input into the output.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Algorithm",
+    answer: "We can also view an algorithm as a tool for solving a well-specified computational problem. The statement of the problem specifies in general terms the desired input/output relationship. The algorithm describes a specific computational procedure for achieving that input/output relationship.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Algorithm",
+    answer: "An algorithm can also be defined as the unambiguous step-by-step instructions to solve a given problem on a formal note.",
+    category: "Chapter_1"
+  },
+  {
+    question: "Correctness and Efficiency",
+    answer: "In the formal study of algorithms, there are two main criteria for judging the merits of algorithms: (does the algorithm give a solution to the problem in a finite number of steps?) and (how much resources (in terms of memory and time) does it take to execute the).",
+    category: "Chapter_1"
+  },
+  {
+    question: "Given the input sequence: {31; 41; 59; 26; 41; 58}, a _____ algorithm returns as output the sequence {26; 31; 41; 41; 58; 59}. Such an input sequence is called an instance of the sorting problem.",
+    answer: "Sorting",
+    category: "Chapter_1"
+  },
+  {
+    question: "Given the input sequence: {31; 41; 59; 26; 41; 58}, a sorting algorithm returns as output the sequence {26; 31; 41; 41; 58; 59}. Such an input sequence is called an _____. In general, It consists of the input (satisfying whatever constraints are imposed in the problem statement) to compute a solution to the problem.",
+    answer: "Instance of the Sorting Problem",
+    category: "Chapter_1"
+  },
+  {
+    question: "Because many programs use it as an intermediate step, _____ is a fundamental operation in computer science. As a result, we have a large number of good _____ algorithms at our disposal.",
+    answer: "Sorting",
+    category: "Chapter_1"
+  },
+  {
+    question: "An algorithm can be specified in English, as a _____, or even a hardware design. The only requirement is that the specification must provide an accurate description of the computational procedure to be followed.",
+    answer: "Computer Program",
+    category: "Chapter_1"
+  },
+  {
+    question: "Helps us to determine which algorithm is most efficient in terms of time and space consumed. The goal is to compare algorithms (or solutions) mainly in terms of running time and other factors (e.g., memory, developer effort, etc.).",
+    answer: "Algorithm Analysis",
+    category: "Chapter_1"
+  },
+  {
+    question: "Multiple algorithms are available for solving the same problem (for example, a sorting problem has many algorithms, like insertion sort, selection sort, quick sort, and many more).",
+    answer: "True",
+    category: "Chapter_1"
+  },
+  {
+    question: "Which algorithm is best for a given application depends on—among other factors—the number of items to be sorted, the extent to which the items are already somewhat sorted, possible restrictions on the item values, the architecture of the computer, and the kind of storage devices to be used: main memory, disks, or even tapes.",
+    answer: "True",
+    category: "Chapter_1"
+  },
+  {
+    question: "An algorithm is said to be correct if, for every input instance, it halts with the correct output. An incorrect algorithm might not halt at all on some input instances, or it might stop with a wrong answer.",
+    answer: "True",
+    category: "Chapter_1"
+  },
+  {
+    question: "We say that a correct algorithm solves the given computational problem. Contrary to what you might expect, faulty algorithms can sometimes be helpful, if we can control their error rate.",
+    answer: "True",
+    category: "Chapter_1"
   }
 ];
-
-export default science_questions;
