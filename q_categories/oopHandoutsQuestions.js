bd24f4f006c3061add5b5558f0d9695b796822cf
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-01-24 20:12:39 +0800

    Create oopHandoutsQuestions.js

---
+++ q_categories/oopHandoutsQuestions.js
@@ -0,0 +1,79 @@
+const oopHandouts_questions = [
+  {
+    answer: "An _____ has characteristics or attributes.",
+    question: "Object",
+    category: "Handouts"
+  },
+  {
+    answer: "The values of an object’s attributes give the object a state.",
+    question: "True",
+    category: "Handouts"
+  },
+  {
+    answer: "The actions that an object can take are called _____.",
+    question: "Behaviors",
+    category: "Handouts"
+  },
+  {
+    answer: "Each behavior is defined by a piece of Java code called a _____.",
+    question: "Method",
+    category: "Handouts"
+  },
+  {
+    answer: "Objects of the same kind are said to have the same data type and belong to the same class.",
+    question: "True",
+    category: "Handouts"
+  },
+  {
+    answer: "A _____defines a kind of object; it is a blueprint for defining the objects.",
+    question: "Class",
+    category: "Handouts"
+  },
+  {
+    answer: "The _____ of an object is the name of its class.",
+    question: "Data Type",
+    category: "Handouts"
+  },
+  {
+    answer: "One way of organizing classes is through _____ .",
+    question: "Inheritance",
+    category: "Handouts"
+  },
+  {
+    answer: "_____ allows objects of a class to take on the properties of objects from another class.",
+    question: "Inheritance",
+    category: "Handouts"
+  },
+  {
+    answer: "_____ is used to avoid the repetition of programming instructions for each class.",
+    question: "Inheritance",
+    category: "Handouts"
+  },
+  {
+    answer: "To apply inheritance between classes, the _____ keyword is used.",
+    question: "Extends",
+    category: "Handouts"
+  },
+  {
+    answer: "An _____ is a program component that contains the heading for a number of public methods.",
+    question: "Interface",
+    category: "Handouts"
+  },
+  {
+    answer: "Some _____ describe all the public methods in a class while others specify only certain methods.",
+    question: "True",
+    category: "Handouts"
+  },
+  {
+    answer: "An interface is used by another class through the _____ keyword.",
+    question: "Implements",
+    category: "Handouts"
+  },
+  {
+    answer: "A _____ is a collection of related classes and interfaces that have been grouped together into a folder.",
+    question: "Package",
+    category: "Handouts"
+  }
+];
+
+export default oopHandouts_questions;
