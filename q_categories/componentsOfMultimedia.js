92be524f9656a9a1b6694a3f25e32bdce10aaecb
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 16:52:18 +0800

    Create componentsOfMultimedia.js

---
+++ q_categories/componentsOfMultimedia.js
@@ -0,0 +1,34 @@
+const componentsOfMultimedia_questions = [
+  {
+    question: "What is written content that can be displayed on screens or read aloud?",
+    answer: "Text",
+    category: "componentsOfMultimedia"
+  },
+  {
+    question: "What refers to sound or music that enhances the auditory experience?",
+    answer: "Audio",
+    category: "componentsOfMultimedia"
+  },
+  {
+    question: "What are still visuals, such as photographs or graphics?",
+    answer: "Images",
+    category: "componentsOfMultimedia"
+  },
+  {
+    question: "What are moving images that can include a combination of audio and visual elements?",
+    answer: "Video",
+    category: "componentsOfMultimedia"
+  },
+  {
+    question: "What are dynamic and interactive sequences of images or objects?",
+    answer: "Animations",
+    category: "componentsOfMultimedia"
+  },
+  {
+    question: "What are buttons, links, or other features that allow user engagement?",
+    answer: "Interactive Elements",
+    category: "componentsOfMultimedia"
+  }
+];
+
+export default componentsOfMultimedia_questions;
