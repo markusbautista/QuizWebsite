13cafa57dc4eaea66c1d8b8ac3795e0a9a8b88e2
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 19:40:15 +0800

    Create interactiveSystemDesignPhases.js

---
+++ q_categories/interactiveSystemDesignPhases.js
@@ -0,0 +1,64 @@
+const interactiveSystemDesignPhases_questions = [
+  {
+    question: "What outlines the functional and non-functional requirements that a system must meet to satisfy user needs and expectations, serving as a blueprint for system development?",
+    answer: "Requirement Specification",
+    category: "interactiveSystemDesignPhases"
+  },
+  {
+    question: "In system development, what refers to the process of clearly defining and documenting the needs, functionalities, constraints, and expectations of a system being developed?",
+    answer: "Requirement Specification",
+    category: "interactiveSystemDesignPhases"
+  },
+  {
+    question: "What plays an important role in ensuring that the final product meets user needs, functions as intended, and complies with relevant standards and constraints?",
+    answer: "Requirement Specification",
+    category: "interactiveSystemDesignPhases"
+  },
+  {
+    question: "Overall, what is a critical process in system development that lays the groundwork for the successful design, development, and deployment of a system, involving gathering, analyzing, prioritizing, and documenting requirements?",
+    answer: "Requirement Specification",
+    category: "interactiveSystemDesignPhases"
+  },
+  {
+    question: "What refers to the process of defining the overall structure and organization of an interactive system to support its functionality, usability, and performance?",
+    answer: "Architectural Design",
+    category: "interactiveSystemDesignPhases"
+  },
+  {
+    question: "What involves making high-level decisions about the system's components, interfaces, interactions, and the distribution of responsibilities among these components?",
+    answer: "Architectural Design",
+    category: "interactiveSystemDesignPhases"
+  },
+  {
+    question: "What is concerned with understanding how a system should be organized and designing its overall structure?",
+    answer: "Architectural Design",
+    category: "interactiveSystemDesignPhases"
+  },
+  {
+    question: "What phase of the design process involves completing the product's design, translating the overall system architecture and functionality into specific, concrete designs for user interfaces, interactions, and system components?",
+    answer: "Detailed Design",
+    category: "interactiveSystemDesignPhases"
+  },
+  {
+    question: "What phase of the design process is characterized by the design team working toward completion of the specifications for the product and its subassemblies, product elements, and manufacturing processes?",
+    answer: "Detailed Design",
+    category: "interactiveSystemDesignPhases"
+  },
+  {
+    question: "What is a software development process in which program units are combined and tested as groups in multiple ways?",
+    answer: "Integration and Testing",
+    category: "interactiveSystemDesignPhases"
+  },
+  {
+    question: "In which integration testing method does the testing begin with unit testing, followed by tests of progressively higher-level combinations of units called modules or builds?",
+    answer: "Bottom-up integration testing",
+    category: "interactiveSystemDesignPhases"
+  },
+  {
+    question: "In which integration testing method are the highest-level modules tested first, followed by progressively lower-level modules?",
+    answer: "Top-down integration testing",
+    category: "interactiveSystemDesignPhases"
+  }
+];
+
+export default interactiveSystemDesignPhases_questions;
