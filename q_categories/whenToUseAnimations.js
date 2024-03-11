b4c8e8a6755cbd25e57b133107f31c4b7c209a30
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 17:07:25 +0800

    Create whenToUseAnimations.js

---
+++ q_categories/whenToUseAnimations.js
@@ -0,0 +1,39 @@
+const whenToUseAnimations_questions = [
+  {
+    question: "When should animations be used to make interfaces dynamic and engaging?",
+    answer: "Enhance User Experience",
+    category: "whenToUseAnimations"
+  },
+  {
+    question: "When should animations be implemented as visual cues for navigation or performing tasks?",
+    answer: "Guide User Behavior",
+    category: "whenToUseAnimations"
+  },
+  {
+    question: "When should animations be used to give users immediate feedback on their actions?",
+    answer: "Provide Feedback",
+    category: "whenToUseAnimations"
+  },
+  {
+    question: "When should animations be implemented to ease transitions and help users understand changes in the interface?",
+    answer: "Reduce Cognitive Load",
+    category: "whenToUseAnimations"
+  },
+  {
+    question: "When should subtle animations be used to add a layer of emotion or personality to your design?",
+    answer: "Create an Emotional Connection",
+    category: "whenToUseAnimations"
+  },
+  {
+    question: "When can animations be used to simplify complex processes or concepts for better understanding?",
+    answer: "Illustrate Complex Processes",
+    category: "whenToUseAnimations"
+  },
+  {
+    question: "When should consistent animation styles be used to reinforce brand identity and messaging?",
+    answer: "Reinforce Brand Identity",
+    category: "whenToUseAnimations"
+  }
+];
+
+export default whenToUseAnimations_questions;
