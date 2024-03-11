2e4c92a0b37e8465c37493997469328d15d6c152
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 19:48:54 +0800

    Create architecturalDesign.js

---
+++ q_categories/architecturalDesign.js
@@ -0,0 +1,44 @@
+const architecturalDesign_questions = [
+  {
+    question: "In Architectural Design, What involves identifying components, modules, and their relationships?",
+    answer: "Component Identification",
+    category: "architecturalDesign"
+  },
+  {
+    question: "In Architectural Design, What refers to breaking down a system into small units, organizing complexity, and defining boundaries?",
+    answer: "System Decomposition",
+    category: "architecturalDesign"
+  },
+  {
+    question: "In Architectural Design, What describes the interactions of components with each other?",
+    answer: "Component Communication",
+    category: "architecturalDesign"
+  },
+  {
+    question: "In Architectural Design, What architectural concept involves layers representing different levels?",
+    answer: "Layered Architecture",
+    category: "architecturalDesign"
+  },
+  {
+    question: "In Architectural Design, What addresses the ability of a system to handle processing and loads effectively?",
+    answer: "Scalability and Performance",
+    category: "architecturalDesign"
+  },
+  {
+    question: "In Architectural Design, What aspect focuses on protecting a system from unauthorized access, data breaches, and threats?",
+    answer: "Security",
+    category: "architecturalDesign"
+  },
+  {
+    question: "In Architectural Design, What involves designing a system to accommodate future changes and enhancements?",
+    answer: "Flexibility and Extensibility",
+    category: "architecturalDesign"
+  },
+  {
+    question: "In Architectural Design, What refers to the capability of a system to work with other systems or services?",
+    answer: "Integration with External Systems",
+    category: "architecturalDesign"
+  }
+];
+
+export default architecturalDesign_questions;
