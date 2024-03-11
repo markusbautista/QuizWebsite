19e190a3cda23f799de8fee595d5e3a52803d31c
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 19:45:51 +0800

    Create requirementSpecification.js

---
+++ q_categories/requirementSpecification.js
@@ -0,0 +1,34 @@
+const requirementSpecification_questions = [
+  {
+    question: "In Requirement Specification, What is the task of the system?",
+    answer: "Functional Requirement",
+    category: "requirementSpecification"
+  },
+  {
+    question: "In Requirement Specification, What are the attributes or overall characteristics?",
+    answer: "Non-Functional Requirements",
+    category: "requirementSpecification"
+  },
+  {
+    question: "In Requirement Specification, What is the need or expectation of the user?",
+    answer: "User Requirements",
+    category: "requirementSpecification"
+  },
+  {
+    question: "In Requirement Specification, What includes hardware, software, and infrastructure?",
+    answer: "System Requirements",
+    category: "requirementSpecification"
+  },
+  {
+    question: "In Requirement Specification, What are the limitations and restrictions?",
+    answer: "Constraints",
+    category: "requirementSpecification"
+  },
+  {
+    question: "In Requirement Specification, What provides information from users?",
+    answer: "Use Cases or User Stories",
+    category: "requirementSpecification"
+  }
+];
+
+export default requirementSpecification_questions;
