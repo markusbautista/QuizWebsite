c0893ab0fdee0d9a6ac2635a28582a5af0b26fdf
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 16:06:57 +0800

    Create ist_questions.js

---
+++ ist_questions.js
@@ -0,0 +1,11 @@
+import art_questions from './q_categories/art.js';
+import history_questions from './q_categories/history.js';
+import science_questions from './q_categories/science.js';
+
+const questions = [
+  ...science_questions,
+  ...history_questions,
+  ...art_questions
+];
+
+export default questions;
