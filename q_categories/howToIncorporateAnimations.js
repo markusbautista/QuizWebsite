b872eb1627f50d686ac1a52db3fae6ba3f026dca
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 17:09:31 +0800

    Create howToIncorporateAnimations.js

---
+++ q_categories/howToIncorporateAnimations.js
@@ -0,0 +1,34 @@
+const howToIncorporateAnimations_questions = [
+  {
+    question: "What should ensure that each animation serves a clear purpose, aiding storytelling, clarifying interactions, or guiding users through design?",
+    answer: "Purpose and Relevance",
+    category: "howToIncorporateAnimations"
+  },
+  {
+    question: "What should animations aim for to avoid confusing users or slowing down the interface?",
+    answer: "Simplicity and Clarity",
+    category: "howToIncorporateAnimations"
+  },
+  {
+    question: "What should animations be optimized for to ensure quick loading and smooth execution, especially on mobile devices?",
+    answer: "Performance and Responsiveness",
+    category: "howToIncorporateAnimations"
+  },
+  {
+    question: "What should be maintained throughout the application or website to create a cohesive user experience?",
+    answer: "Consistency",
+    category: "howToIncorporateAnimations"
+  },
+  {
+    question: "How can animations be used to provide feedback on user actions, such as clicking a button?",
+    answer: "Feedback and Interaction",
+    category: "howToIncorporateAnimations"
+  },
+  {
+    question: "What should be considered to accommodate users with sensitivities to motion or those who rely on assistive technologies?",
+    answer: "Accessibility",
+    category: "howToIncorporateAnimations"
+  }
+];
+
+export default howToIncorporateAnimations_questions;
