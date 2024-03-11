f0c9285f0fbc8a09354e39115e20be8fe35248a8
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 17:18:27 +0800

    Update ist_questions.js

--- ist_questions.js
+++ ist_questions.js
@@ -3,13 +3,16 @@ import whenToUseAnimations_questions from './q_categories/whenToUseAnimations.js
 import howToIncorporateAnimations_questions from './q_categories/howToIncorporateAnimations.js';
 import principlesOfAnimations_questions from './q_categories/principlesOfAnimations.js';
 import interactiveSystemDesign_questions from './q_categories/interactiveSystemDesign.js';
+import interactiveSystemDesignLifeCycle_questions from './q_categories/interactiveSystemDesignLifeCycle.js';
+import art_questions from './q_categories/art.js';
 
 const questions = [
   ...componentsOfMultimedia_questions,
   ...whenToUseAnimations_questions,
   ...howToIncorporateAnimations_questions,
   ...principlesOfAnimations_questions,
-  ...interactiveSystemDesign_questions
+  ...interactiveSystemDesign_questions,
+  ...interactiveSystemDesignLifeCycle_questions
 ];
 
 export default questions;
