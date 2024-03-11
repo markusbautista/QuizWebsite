ee0056e6d294fe817b8963a09a9302ea578f48ab
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 17:02:56 +0800

    Update ist_questions.js

--- ist_questions.js
+++ ist_questions.js
@@ -1,7 +1,9 @@
 import componentsOfMultimedia_questions from './q_categories/componentsOfMultimedia.js';
+import art_questions from './q_categories/art.js';
 
 const questions = [
-  ...componentsOfMultimedia_questions
+  ...componentsOfMultimedia_questions,
+  ...art_questions
 ];
 
 export default questions;
