22b323b052b666b3d561010679c59d91d012af2b
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 19:50:58 +0800

    Create detailedDesign.js

---
+++ q_categories/detailedDesign.js
@@ -0,0 +1,34 @@
+const detailedDesign_questions = [
+  {
+    question: "What is GUI design which users interact with the system?",
+    answer: "User Interface Design",
+    category: "detailedDesign"
+  },
+  {
+    question: "What is how users will interact to accomplish tasks (input methods)?",
+    answer: "Interaction Design",
+    category: "detailedDesign"
+  },
+  {
+    question: "What is organized and structure the content and functionality?",
+    answer: "Information Architecture",
+    category: "detailedDesign"
+  },
+  {
+    question: "What is creating representations of interface and interactions?",
+    answer: "Prototyping",
+    category: "detailedDesign"
+  },
+  {
+    question: "What ensures systems can be used based on human capabilities?",
+    answer: "Accessibility",
+    category: "detailedDesign"
+  },
+  {
+    question: "What specifies data structures, algorithms, software modules and other technical matters?",
+    answer: "Technical Design",
+    category: "detailedDesign"
+  }
+];
+
+export default detailedDesign_questions;
