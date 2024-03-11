546bdbeeff688316bab9bd92c3f53b05e44fde96
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 19:20:25 +0800

    Create testingPhases.js

---
+++ q_categories/testingPhases.js
@@ -0,0 +1,29 @@
+const testingPhases_questions = [
+  {
+    question: "What involves testing individual components or modules of the interactive system in isolation to ensure they function correctly according to their specifications?",
+    answer: "Unit Testing",
+    category: "testingPhases"
+  },
+  {
+    question: "What verifies that different components of the interactive system work together correctly when integrated?",
+    answer: "Integration Testing",
+    category: "testingPhases"
+  },
+  {
+    question: "What focuses on verifying the correctness and functionality of the user interface components and interactions?",
+    answer: "UI Testing",
+    category: "testingPhases"
+  },
+  {
+    question: "What ensures that the interactive system is usable by individuals with disabilities?",
+    answer: "Accessibility Testing",
+    category: "testingPhases"
+  },
+  {
+    question: "What involves testing the interactive system with end users to validate that it meets their needs and expectations?",
+    answer: "User Acceptance Testing (UAT)",
+    category: "testingPhases"
+  }
+];
+
+export default testingPhases_questions;
