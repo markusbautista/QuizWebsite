8623bbbf36af293d321dc536865a98adce79a891
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 17:11:41 +0800

    Create principlesOfAnimations.js

---
+++ q_categories/principlesOfAnimations.js
@@ -0,0 +1,59 @@
+const principlesOfAnimations_questions = [
+  {
+    question: "What is essential to create animations with the illusion of gravity, weight, mass, and flexibility?",
+    answer: "Squash and Stretch",
+    category: "principlesOfAnimations"
+  },
+  {
+    question: "What helps to prepare the viewer for what's about to happen, making any motion feel real?",
+    answer: "Anticipation",
+    category: "principlesOfAnimations"
+  },
+  {
+    question: "What describes motions that guide the viewer's eye and draw attention to what's important within the scene?",
+    answer: "Staging",
+    category: "principlesOfAnimations"
+  },
+  {
+    question: "What refers to two different approaches to animation, with one drawing frame-by-frame from start to finish?",
+    answer: "Straight-Ahead Action and Pose to Pose",
+    category: "principlesOfAnimations"
+  },
+  {
+    question: "What reflects the fact that not all parts of an object move at the same time?",
+    answer: "Follow Through and Overlapping Action",
+    category: "principlesOfAnimations"
+  },
+  {
+    question: "What defines how movement starts and stops, with examples like a vehicle starting to move slowly before accelerating?",
+    answer: "Slow In and Slow Out",
+    category: "principlesOfAnimations"
+  },
+  {
+    question: "In the physical world, how do most objects move?",
+    answer: "Arc",
+    category: "principlesOfAnimations"
+  },
+  {
+    question: "What are used to support or emphasize the main action going on within a scene, adding another dimension to animations?",
+    answer: "Secondary Action",
+    category: "principlesOfAnimations"
+  },
+  {
+    question: "What gives animations a fun character and helps the viewer focus on what’s important?",
+    answer: "Exaggeration",
+    category: "principlesOfAnimations"
+  },
+  {
+    question: "Understanding the basics of drawing—or, in the case of UI, the principles of visual design—will help designers create more successful animations. What principle does this refer to?",
+    answer: "Solid Drawing",
+    category: "principlesOfAnimations"
+  },
+  {
+    question: "What do animations need to do in order to trigger the viewer's attention and create a pull?",
+    answer: "Appeal",
+    category: "principlesOfAnimations"
+  }
+];
+
+export default principlesOfAnimations_questions;
