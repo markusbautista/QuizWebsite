3fd38051cf3e12ab36167a94aecd001d1827e6c4
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 17:13:48 +0800

    Create interactiveSystemDesign.js

---
+++ q_categories/interactiveSystemDesign.js
@@ -0,0 +1,54 @@
+const interactiveSystemDesign_questions = [
+  {
+    question: "What involves designing interactive systems with the needs, preferences, and capabilities of users at the forefront?",
+    answer: "User-Centered Design",
+    category: "interactiveSystemDesign"
+  },
+  {
+    question: "What is a critical aspect of interactive system design, focusing on the ease of learning, efficiency of use, and user satisfaction?",
+    answer: "Usability",
+    category: "interactiveSystemDesign"
+  },
+  {
+    question: "What is the point of interaction between the user and the system, involving visually appealing layouts and intuitive navigation structures?",
+    answer: "User Interface Design",
+    category: "interactiveSystemDesign"
+  },
+  {
+    question: "What involves organizing and structuring information within the system to make it easy for users to find and understand?",
+    answer: "Information Architecture",
+    category: "interactiveSystemDesign"
+  },
+  {
+    question: "What focuses on defining how users interact with the system and how the system responds to user input?",
+    answer: "Interaction Design",
+    category: "interactiveSystemDesign"
+  },
+  {
+    question: "What ensures interactive systems are accessible to users of all abilities, including those with disabilities?",
+    answer: "Accessibility",
+    category: "interactiveSystemDesign"
+  },
+  {
+    question: "What ensures interactive systems adapt to different screen sizes and resolutions, providing a consistent user experience across devices?",
+    answer: "Responsive Design",
+    category: "interactiveSystemDesign"
+  },
+  {
+    question: "What is an iterative process of prototyping, testing, and refining design solutions based on user feedback?",
+    answer: "Iterative Design Process",
+    category: "interactiveSystemDesign"
+  },
+  {
+    question: "What involves gathering feedback from users and analyzing usage data to improve interactive systems?",
+    answer: "User Feedback and Analytics",
+    category: "interactiveSystemDesign"
+  },
+  {
+    question: "What often requires collaboration between designers, developers, and stakeholders from various disciplines?",
+    answer: "Cross-Disciplinary Collaboration",
+    category: "interactiveSystemDesign"
+  }
+];
+
+export default interactiveSystemDesign_questions;
