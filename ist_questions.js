8548039a838258752442293d1807128cc2c78afa
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-11 17:10:35 +0800

    Update ist_questions.js

--- ist_questions.js
+++ ist_questions.js
@@ -1,9 +1,11 @@
 import componentsOfMultimedia_questions from './q_categories/componentsOfMultimedia.js';
 import whenToUseAnimations_questions from './q_categories/whenToUseAnimations.js';
+import howToIncorporateAnimations_questions from './q_categories/howToIncorporateAnimations.js';
 
 const questions = [
   ...componentsOfMultimedia_questions,
-  ...whenToUseAnimations_questions
+  ...whenToUseAnimations_questions,
+  ...howToIncorporateAnimations_questions
 ];
 
 export default questions;
