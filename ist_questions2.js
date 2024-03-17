fb5523e75e840cfec057269ea453f23328b6e62b
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-17 10:40:32 +0800

    Create ist_questions2.js

---
+++ ist_questions2.js
@@ -0,0 +1,20 @@
+import componentsOfMultimedia_questions from './q_categories/componentsOfMultimedia.js';
+import whenToUseAnimations_questions from './q_categories/whenToUseAnimations.js';
+import howToIncorporateAnimations_questions from './q_categories/howToIncorporateAnimations.js';
+import principlesOfAnimations_questions from './q_categories/principlesOfAnimations.js';
+import interactiveSystemDesign_questions from './q_categories/interactiveSystemDesign.js';
+import interactiveSystemDesignPhases_questions from './q_categories/interactiveSystemDesignPhases.js';
+import interactiveSystemDesignLifeCycle_questions from './q_categories/interactiveSystemDesignLifeCycle.js';
+import testingPhases_questions from './q_categories/testingPhases.js';
+import requirementSpecification_questions from './q_categories/requirementSpecification.js';
+import architecturalDesign_questions from './q_categories/architecturalDesign.js';
+import detailedDesign_questions from './q_categories/detailedDesign.js';
+import trueOrFalse_questions from './q_categories/trueOrFalse.js';
+
+
+
+const questions = [
+  ...trueOrFalse_questions
+];
+
+export default questions;
