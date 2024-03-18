b7b39a03dd7eb8369c432131d7f19bcc42a17db1
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-18 10:56:43 +0800

    Create generalQuestions.js

---
+++ q_categories/generalQuestions.js
@@ -0,0 +1,84 @@
+const generalQuestions_questions = [
+  {
+    question: "These are computers where people and computers interact a lot. Examples include: Macintosh or Windows OS, editors, CAD-CAM, games, web browsers, and IDEs.",
+    answer: "Interactive Sytems",
+    category: "generalQuestions"
+  },
+  {
+    question: "These were the first interactive systems. In these systems, the interaction between the person and the computer was strict. Examples includ: UNIX and DOS.",
+    answer: "Command Line Systems",
+    category: "generalQuestions"
+  },
+  {
+    question: "A second generation of systems that used menus, forms, and dialogs, which made it easier on memory which replaced command line interfaces. Examples include: ATMs",
+    answer: "Form-based Systems",
+    category: "generalQuestions"
+  },
+  {
+    question: "In 1980, Xerox Corporation introduced a third generation of interactive computing. Resulting from six years of research and development, integrated the mouse, icons, the desktop metaphor, windows, and bit-mapped displays to work together.",
+    answer: "Xerox Star",
+    category: "generalQuestions"
+  },
+  {
+    question: "Who made the windows, icon, menu, and pointer (or WIMP) approach universal?",
+    answer: "Microsoft Windows",
+    category: "generalQuestions"
+  },
+  {
+    question: "The Xerox Star was replicated in the Lisa and Macintosh first offered by?",
+    answer: "Apple Inc.",
+    category: "generalQuestions"
+  },
+  {
+    question: "Who described the interaction between humans and computers in terms of: gulfs of execution and evaluation.",
+    answer: "Professor Donald A. Norman",
+    category: "generalQuestions"
+  },
+  {
+    question: "Studies have led to principles for: _____ related to the limits of short-term memory",
+    answer: "Menu Construction",
+    category: "generalQuestions"
+  },
+  {
+    question: "Studies have led to principles for: _____ based on metaphors that activate areas of long-term memory",
+    answer: "System Design",
+    category: "generalQuestions"
+  },
+  {
+    question: "Studies have led to principles for: _____ of buttons and icons, based on studies of motor skills",
+    answer: "Target Size",
+    category: "generalQuestions"
+  },
+  {
+    question: "Studies have led to principles for: the use of _____ based on human sensory capabilities and limits.",
+    answer: "Visual and Auditory Cues",
+    category: "generalQuestions"
+  },
+  {
+    question: "Is a system in which the user is able to select an object and then specify which actions are to be taken.",
+    answer: "Direct Manipulation",
+    category: "generalQuestions"
+  },
+  {
+    question: "Are systems where the user would normally specify an action and then select an object upon which the action was to be performed.",
+    answer: "Command Line Systems",
+    category: "generalQuestions"
+  },
+  {
+    question: "Are systems where the user would normally specify an action and then select an object upon which the action was to be performed.",
+    answer: "Command Line Systems",
+    category: "generalQuestions"
+  },
+  {
+    question: "With the invention of direct manipulation systems, the basic programming paradigm had to change from the process-driven approach to an _____ perspective.",
+    answer: "Event-driven",
+    category: "generalQuestions"
+  },
+  {
+    question: "This refers to the use of multiple forms of media, such as text, audio, images, animations, and video, to convey information or create an interactive and engaging experience.",
+    answer: "Multimedia",
+    category: "generalQuestions"
+  }
+];
+
+export default generalQuestions_questions;
