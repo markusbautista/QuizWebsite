8cae54c9fef1b9602f09c2cfe752a8b27c699a39
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-19 22:21:31 +0800

    Update ist_questions2.js

--- ist_questions2.js
+++ ist_questions2.js
@@ -1,7 +1,7 @@
-import componentsOfMultimedia_questions from './q_categories/componentsOfMultimedia.js';
+import adobeAnimateShortcuts_questions from './q_categories/adobeAnimateShortcuts.js';
 
 const questions = [
-  ...
+  ...adobeAnimateShortcuts_questions
 ];
 
 export default questions;
