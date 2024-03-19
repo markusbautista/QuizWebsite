965e6998652518001fdbc237681e9008d35309da
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-20 06:19:36 +0800

    Create adobeAnimateFileTypes.js

---
+++ q_categories/adobeAnimateFileTypes.js
@@ -0,0 +1,44 @@
+const adobeAnimateFileTypes_questions = [
+  {
+    question: "What are the primary files you work with in Animate, containing the basic media, timeline, and script information for an Animate document?",
+    answer: "FLA files",
+    category: "adobeAnimateFileTypes"
+  },
+  {
+    question: "What type of files are similar to FLA files and are simply the uncompressed equivalent of a FLA file, making it easier for groups of users to work on different elements of an Animate project at the same time?",
+    answer: "Uncompressed XFL files",
+    category: "adobeAnimateFileTypes"
+  },
+  {
+    question: "What are the compiled versions of FLA files, which are the files displayed in a web page when you publish your FLA file?",
+    answer: "SWF files",
+    category: "adobeAnimateFileTypes"
+  },
+  {
+    question: "What type of files are ActionScript files used to keep some or all of your ActionScript code outside of your FLA files, helpful for code organization and for projects with multiple people working on different parts of the Animate content?",
+    answer: "AS files",
+    category: "adobeAnimateFileTypes"
+  },
+  {
+    question: "What type of files contain the reusable Animate components, each containing a compiled movie clip, ActionScript code, and any other assets that the component requires?",
+    answer: "SWC files",
+    category: "adobeAnimateFileTypes"
+  },
+  {
+    question: "What type of files are used to store ActionScript that will be executed on a computer running Adobe Media Server, providing the ability to implement server-side logic that works in conjunction with ActionScript in a SWF file?",
+    answer: "ASC files",
+    category: "adobeAnimateFileTypes"
+  },
+  {
+    question: "What type of JavaScript files can you use to add new functionality to the Animate authoring tool?",
+    answer: "JSFL files",
+    category: "adobeAnimateFileTypes"
+  },
+  {
+    question: "What type of files let you bundle the canvas publish template along with its publish profile settings, bundling and sharing any new asset linked to a publish profile going forward?",
+    answer: "APR files",
+    category: "adobeAnimateFileTypes"
+  }
+];
+
+export default adobeAnimateFileTypes_questions;
