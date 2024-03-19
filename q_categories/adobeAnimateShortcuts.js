6857cce38584e34b9467a2646b2f1a6f855e0d6a
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-19 22:20:45 +0800

    Create adobeAnimateShortcuts.js

---
+++ q_categories/adobeAnimateShortcuts.js
@@ -0,0 +1,134 @@
+const adobeAnimateShortcuts_questions = [
+  {
+    question: "What is the shortcut for Selection?",
+    answer: "V",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Sub-Selection?",
+    answer: "A",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Free Transform?",
+    answer: "Q",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for 3D-Rotation?",
+    answer: "Shift + W",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Lasso?",
+    answer: "L",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Pen?",
+    answer: "P",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Text?",
+    answer: "T",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Line?",
+    answer: "N",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Rectangle?",
+    answer: "R",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Oval?",
+    answer: "O",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for PolyStar?",
+    answer: "PolyStar",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Pencil?",
+    answer: "Shift + Y",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Paint Brush?",
+    answer: "Y",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Brush?",
+    answer: "B",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Bone?",
+    answer: "M",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Paint Bucket?",
+    answer: "K",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Gradient Transform?",
+    answer: "F",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for 3D-Translation?",
+    answer: "G",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Polygon?",
+    answer: "L",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Add Anchor Point?",
+    answer: "=+",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Rectangle Primitive?",
+    answer: "R",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Oval Primitive?",
+    answer: "O",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Bind?",
+    answer: "M",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Magic Wand?",
+    answer: "L",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Delete Anchor Point?",
+    answer: "-_",
+    category: "adobeAnimateShortcuts"
+  },
+  {
+    question: "What is the shortcut for Convert Icon Point?",
+    answer: "Shift + C",
+    category: "adobeAnimateShortcuts"
+  }
+];
+
+export default adobeAnimateShortcuts_questions;
