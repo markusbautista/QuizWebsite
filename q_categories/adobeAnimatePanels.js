aac82eb873b931924f5ee3a4ca1b7be7bbc984f3
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-20 06:22:11 +0800

    Create adobeAnimatePanels.js

---
+++ q_categories/adobeAnimatePanels.js
@@ -0,0 +1,39 @@
+const adobeAnimatePanels_questions = [
+  {
+    question: "What contains 11 fundamental menus that reveal many options and commands?",
+    answer: "Menu bar",
+    category: "adobeAnimatePanels"
+  },
+  {
+    question: "What is the main panel used to organize and control movie animation contents over time, along with layers and frames?",
+    answer: "Timeline",
+    category: "adobeAnimatePanels"
+  },
+  {
+    question: "By moving the Playhead to a specific frame in the Timeline, what can be displayed?",
+    answer: "Specific content within the frame",
+    category: "adobeAnimatePanels"
+  },
+  {
+    question: "What contains tools for selecting, drawing, painting, editing, and navigating elements within the stage?",
+    answer: "Tools Panel",
+    category: "adobeAnimatePanels"
+  },
+  {
+    question: "What is used to manipulate or modify properties of selected elements or the document itself, displaying options like dimensions, color, size, etc.?",
+    answer: "Properties Inspector / Properties Panel",
+    category: "adobeAnimatePanels"
+  },
+  {
+    question: "What is the big white area used to insert text, images, and video that appear on the screen, and where animations or drawings are created?",
+    answer: "Stage",
+    category: "adobeAnimatePanels"
+  },
+  {
+    question: "What is the big background gray area around the stage where elements can be placed but won't be visible to the audience when the movie is played and/or compressed?",
+    answer: "Pasteboard",
+    category: "adobeAnimatePanels"
+  }
+];
+
+export default adobeAnimatePanels_questions;
