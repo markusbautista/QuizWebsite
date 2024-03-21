9a182dcfc07b7c9be34dcea6011a755e06b2ae59
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-21 15:56:11 +0800

    Create sqlStatements.js

---
+++ q_categories/sqlStatements.js
@@ -0,0 +1,74 @@
+const sqlStatements_questions = [
+  {
+    question: "These are statements that define, modify, and destroy the structure of the database and the tables within the database.",
+    answer: "Data Definition Language (DDL) Statements",
+    category: "sqlStatements"
+  },
+  {
+    question: "Creates a new database. DDL",
+    answer: "CREATE DATABASE",
+    category: "sqlStatements"
+  },
+  {
+    question: "Removes an existing database. DDL",
+    answer: "DROP DATABASE",
+    category: "sqlStatements"
+  },
+  {
+    question: "Creates a new database table. DDL",
+    answer: "CREATE TABLE",
+    category: "sqlStatements"
+  },
+  {
+    question: "Modifies the structure of a database table. DDL",
+    answer: "ALTER TABLE",
+    category: "sqlStatements"
+  },
+  {
+    question: "Destroys a database table. DDL",
+    answer: "DROP TABLE",
+    category: "sqlStatements"
+  },
+  {
+    question: "Used to control the contents of the tables and do not have an effect on the structure of the database.",
+    answer: "Data Manipulation Language (DML) Statements",
+    category: "sqlStatements"
+  },
+  {
+    question: "Inserts new data into the database table. DML",
+    answer: "INSERT INTO",
+    category: "sqlStatements"
+  },
+  {
+    question: "Retrieves data from the database tables. DML",
+    answer: "SELECT",
+    category: "sqlStatements"
+  },
+  {
+    question: "Modifies the data stored in the database tables. DML",
+    answer: "UPDATE",
+    category: "sqlStatements"
+  },
+  {
+    question: "Removes a row/record from a database table. DML",
+    answer: "DELETE",
+    category: "sqlStatements"
+  },
+  {
+    question: "These are statements used to control a database including administering privileges by authorizing certain users to view, change, or delete database objects and/or the data within the database.",
+    answer: "Data Control Language (DCL) Statements",
+    category: "sqlStatements"
+  },
+  {
+    question: "Assigns privileges to users and roles. DCL",
+    answer: "GRANT",
+    category: "sqlStatements"
+  },
+  {
+    question: "Removes privileges. DCL",
+    answer: "REVOKE",
+    category: "sqlStatements"
+  }
+];
+
+export default sqlStatements_questions;
