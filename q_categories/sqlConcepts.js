cc937380c90d136d1d85192dc495046f38e264b1
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-21 15:58:05 +0800

    Update sqlConcepts.js

--- q_categories/sqlConcepts.js
+++ q_categories/sqlConcepts.js
@@ -2,47 +2,47 @@ const sqlConcepts_questions = [
   {
     question: "A word, clause, or statement added by the programmer to remind him of what a particular part of code does (// or /* */).",
     answer: "Comments",
-    category: "sql"
+    category: "sqlConcepts"
   },
   {
     question: "Names given to the database objects such as tables, columns, views, and databases (COLLEGE which is a name given to a table).",
     answer: "Identifiers",
-    category: "sql"
+    category: "sqlConcepts"
   },
   {
     question: "Words with special meaning therefore these are considered reserved and cannot be used as identifiers (SELECT, DATABASE).",
     answer: "Keywords",
-    category: "sql"
+    category: "sqlConcepts"
   },
   {
     question: "A grammatically acceptable and valid combination of indivisible parts such as keywords, identifiers, operators, punctuation marks, or constants of the SQL language (SELECT * FROM COLLEGE).",
     answer: "SQL Statement",
-    category: "sql"
+    category: "sqlConcepts"
   },
   {
     question: "Is a fragment of an SQL statement that is usually introduced by a keyword (ORDER BY FName).",
     answer: "Clause",
-    category: "sql"
+    category: "sqlConcepts"
   },
   {
     question: "Refers to any legal combination of keywords and numeric symbols that evaluates to a single data value (SALARY * 12).",
     answer: "Expression",
-    category: "sql"
+    category: "sqlConcepts"
   },
   {
     question: "Are sections of code that take zero, one, or more input values, perform a specific assignment and return a scalar or tabular set of values (SUM(), UPPER()).",
     answer: "Functions",
-    category: "sql"
+    category: "sqlConcepts"
   },
   {
     question: "Work with one or more simple expressions to form a more complex expression (AND, +, >).",
     answer: "Operators",
-    category: "sql"
+    category: "sqlConcepts"
   },
   {
     question: "Used to end SQL statements. In some variants of the SQL _____ is optional.",
     answer: "Semicolon",
-    category: "sql"
+    category: "sqlConcepts"
   }
 ];
 
