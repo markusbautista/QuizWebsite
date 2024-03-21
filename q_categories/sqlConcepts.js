ba18d729f6b08ad4adfb39f2515131f2a160c062
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-21 15:53:29 +0800

    Create sqlConcepts.js

---
+++ q_categories/sqlConcepts.js
@@ -0,0 +1,49 @@
+const sqlConcepts_questions = [
+  {
+    question: "A word, clause, or statement added by the programmer to remind him of what a particular part of code does (// or /* */).",
+    answer: "Comments",
+    category: "sql"
+  },
+  {
+    question: "Names given to the database objects such as tables, columns, views, and databases (COLLEGE which is a name given to a table).",
+    answer: "Identifiers",
+    category: "sql"
+  },
+  {
+    question: "Words with special meaning therefore these are considered reserved and cannot be used as identifiers (SELECT, DATABASE).",
+    answer: "Keywords",
+    category: "sql"
+  },
+  {
+    question: "A grammatically acceptable and valid combination of indivisible parts such as keywords, identifiers, operators, punctuation marks, or constants of the SQL language (SELECT * FROM COLLEGE).",
+    answer: "SQL Statement",
+    category: "sql"
+  },
+  {
+    question: "Is a fragment of an SQL statement that is usually introduced by a keyword (ORDER BY FName).",
+    answer: "Clause",
+    category: "sql"
+  },
+  {
+    question: "Refers to any legal combination of keywords and numeric symbols that evaluates to a single data value (SALARY * 12).",
+    answer: "Expression",
+    category: "sql"
+  },
+  {
+    question: "Are sections of code that take zero, one, or more input values, perform a specific assignment and return a scalar or tabular set of values (SUM(), UPPER()).",
+    answer: "Functions",
+    category: "sql"
+  },
+  {
+    question: "Work with one or more simple expressions to form a more complex expression (AND, +, >).",
+    answer: "Operators",
+    category: "sql"
+  },
+  {
+    question: "Used to end SQL statements. In some variants of the SQL _____ is optional.",
+    answer: "Semicolon",
+    category: "sql"
+  }
+];
+
+export default sqlConcepts_questions;
