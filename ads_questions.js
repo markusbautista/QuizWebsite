0abd9767dbbfcd907990ad44e8259a45c96c178e
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-21 15:53:58 +0800

    Update ads_questions.js

--- ads_questions.js
+++ ads_questions.js
@@ -1,9 +1,11 @@
 import sql_questions from './q_categories/sqlQuestions.js';
 import sqlDataTypes_questions from './q_categories/sqlDataTypes.js';
+import sqlConcepts_questions from './q_categories/sqlConcepts.js';
 
 const questions = [
   ...sql_questions,
-  ...sqlDataTypes_questions
+  ...sqlDataTypes_questions,
+  ...sqlConcepts_questions
 ];
 
 export default questions;
