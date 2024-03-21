f8b59dd25d0a481b039961c83c30bb1c2a8966f6
markusbautista <141809532+markusbautista@users.noreply.github.com>
2024-03-21 15:58:30 +0800

    Update sqlDataTypes.js

--- q_categories/sqlDataTypes.js
+++ q_categories/sqlDataTypes.js
@@ -2,62 +2,62 @@ const sqlDataTypes_questions = [
   {
     question: "Holds whole numbers",
     answer: "INT",
-    category: "sql"
+    category: "sqlDataTypes"
   },
   {
     question: "Holds numbers containing a decimal point.",
     answer: "DECIMAL(size,d) or NUMERIC(size,d)",
-    category: "sql"
+    category: "sqlDataTypes"
   },
   {
     question: "Holds numbers containing a decimal point and uses 8 bytes for storage with up to 15 digits precision.",
     answer: "FLOAT",
-    category: "sql"
+    category: "sqlDataTypes"
   },
   {
     question: "Holds a string.",
     answer: "VARCHAR(size)",
-    category: "sql"
+    category: "sqlDataTypes"
   },
   {
     question: "Holds a fixed-length character string where the exact length of the data to be stored is already determined.",
     answer: "CHAR(size)",
-    category: "sql"
+    category: "sqlDataTypes"
   },
   {
     question: "Holds date values in YYYY-MM-DD format.",
     answer: "DATE",
-    category: "sql"
+    category: "sqlDataTypes"
   },
   {
     question: "Holds time values in HH:MI:SS.[nnnnnnnnn] format.",
     answer: "SMALL DATETIME",
-    category: "sql"
+    category: "sqlDataTypes"
   },
   {
     question: "Holds date and time values in YYYY-MM-DD HH:MI:SS.[nnn] format.",
     answer: "DATETIME",
-    category: "sql"
+    category: "sqlDataTypes"
   },
   {
     question: "Used to store images in binary format and holds up to 2 gigabytes of binary data.",
     answer: "VARBINARY(max)",
-    category: "sql"
+    category: "sqlDataTypes"
   },
   {
     question: "Used to store images in binary format.",
     answer: "image",
-    category: "sql"
+    category: "sqlDataTypes"
   },
   {
     question: "An integer data type which can store only 0 or 1 and can consume only a single bit of storage space.",
     answer: "bit",
-    category: "sql"
+    category: "sqlDataTypes"
   },
   {
     question: "Used to store xml data.",
     answer: "xml",
-    category: "sql"
+    category: "sqlDataTypes"
   }
 ];
 
